60d21785bdbf1174a0e39191b9e00ba18f2d7278
meredova <olya.gurbo@gmail.com>
2022-08-23 17:51:17 +0300

    feat: add html and css properties

---
+++ index.js
@@ -0,0 +1,122 @@
+const ancientsData = [
+    {
+      id: 'azathoth',
+      name: 'azathoth',
+      //cardFace: Ancients.azathoth,
+      firstStage: {
+        greenCards: 1,
+        blueCards: 1,
+        brownCards: 2,
+      },
+      secondStage: {
+        greenCards: 2,
+        blueCards: 1,
+        brownCards: 3,
+      },
+      thirdStage: {
+        greenCards: 2,
+        blueCards: 0,
+        brownCards: 4,
+      },
+    },
+    {
+      id: 'cthulhu',
+      name: 'cthulhu',
+      //cardFace: Ancients.cthulhu,
+      firstStage: {
+        greenCards: 0,
+        blueCards: 2,
+        brownCards: 2,
+      },
+      secondStage: {
+        greenCards: 1,
+        blueCards: 0,
+        brownCards: 3,
+      },
+      thirdStage: {
+        greenCards: 3,
+        blueCards: 0,
+        brownCards: 4,
+      },
+    },
+    {
+      id: 'iogSothoth',
+      name: 'iogSothoth',
+      //cardFace: Ancients.iogSothoth,
+      firstStage: {
+        greenCards: 0,
+        blueCards: 1,
+        brownCards: 2,
+      },
+      secondStage: {
+        greenCards: 2,
+        blueCards: 1,
+        brownCards: 3,
+      },
+      thirdStage: {
+        greenCards: 3,
+        blueCards: 0,
+        brownCards: 4,
+      },
+    },
+    {
+      id: 'shubNiggurath',
+      name: 'shubNiggurath',
+      //cardFace: Ancients.shubNiggurath,
+      firstStage: {
+        greenCards: 1,
+        blueCards: 1,
+        brownCards: 2,
+      },
+      secondStage: {
+        greenCards: 3,
+        blueCards: 1,
+        brownCards: 2,
+      },
+      thirdStage: {
+        greenCards: 2,
+        blueCards: 0,
+        brownCards: 4,
+      },
+    },
+  ]
+
+const ancients = document.querySelectorAll('.ancient-card');
+const ancientsContainer = document.querySelector('.ancients-container');
+const stages = document.querySelectorAll('.stage');
+
+ancientsContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if(target.classList.contains('ancient-card')) {
+        for(let i = 0; i < ancients.length; i++) {
+            ancients[i].classList.remove('active');
+        }
+        target.classList.add('active');
+    }
+    for(let i = 0; i < ancients.length; i++) {
+        if (ancients[i].classList.contains('active')) {
+            stages[0].textContent = ancientsData[i].firstStage.greenCards;
+            stages[1].textContent = ancientsData[i].firstStage.brownCards;
+            stages[2].textContent = ancientsData[i].firstStage.blueCards;
+            stages[3].textContent = ancientsData[i].secondStage.greenCards;
+            stages[4].textContent = ancientsData[i].secondStage.brownCards;
+            stages[5].textContent = ancientsData[i].secondStage.blueCards;
+            stages[6].textContent = ancientsData[i].thirdStage.greenCards;
+            stages[7].textContent = ancientsData[i].thirdStage.brownCards;
+            stages[8].textContent = ancientsData[i].thirdStage.blueCards;
+        }
+    }
+})
+
+const difficulty = document.querySelectorAll('.difficulty');
+const difficultyContainer = document.querySelector('.difficulty-container');
+
+difficultyContainer.addEventListener('click', (e) => {
+    let target = e.target;
+    if(target.classList.contains('difficulty')) {
+        for(let i = 0; i < difficulty.length; i++) {
+            difficulty[i].classList.remove('active');
+        }
+        target.classList.add('active');
+    }
+})
